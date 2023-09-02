004e0984542d0e4da63b169a0f4c2b8a681dabdd
moticless <moticless@github.com>
2023-09-02 12:44:05 +0300

    Rename file respWriter.c to respToFileWriter.c

--- src/ext/respToFileWriter.c
+++ src/ext/respToFileWriter.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-struct RdbxRespFileWriter {
+struct RdbxRespToFileWriter {
     long cmdCount;
     RdbParser *p;
     FILE *filePtr;   /* either stdout or pointer to open file */
@@ -12,7 +12,7 @@ struct RdbxRespFileWriter {
 /* return 0 for success. 1 Otherwise. */
 static int respFileWritev(void *context, struct iovec *iov, int count, int startCmd, int endCmd) {
     UNUSED(startCmd);
-    struct RdbxRespFileWriter *ctx = context;
+    struct RdbxRespToFileWriter *ctx = context;
     ctx->cmdCount += endCmd;
 
     /* not optimized code */
@@ -28,12 +28,12 @@ static int respFileWritev(void *context, struct iovec *iov, int count, int start
 
 /* return 0 for success. 1 Otherwise. */
 static int respFileFlush(void *context) {
-    struct RdbxRespFileWriter *ctx = context;
+    struct RdbxRespToFileWriter *ctx = context;
     return (EOF == fflush(ctx->filePtr)) ? 1 : 0;
 }
 
 static void respFileWriteDelete(void *context) {
-    struct RdbxRespFileWriter *ctx = context;
+    struct RdbxRespToFileWriter *ctx = context;
     if (ctx != NULL) {
         fflush(ctx->filePtr);
         if (ctx->filePtr != stdout) {
@@ -43,8 +43,8 @@ static void respFileWriteDelete(void *context) {
     }
 }
 
-RdbxRespFileWriter *RDBX_createRespFileWriter(RdbParser *p, RdbxToResp *rdbToResp, const char *filePath) {
-    RdbxRespFileWriter *ctx;
+RdbxRespToFileWriter *RDBX_createRespFileWriter(RdbParser *p, RdbxToResp *rdbToResp, const char *filePath) {
+    RdbxRespToFileWriter *ctx;
     FILE *filePtr;
 
     if (filePath == NULL) {
@@ -58,9 +58,9 @@ RdbxRespFileWriter *RDBX_createRespFileWriter(RdbParser *p, RdbxToResp *rdbToRes
         }
     }
 
-    if ((ctx = RDB_alloc(p, sizeof(RdbxRespFileWriter))) == NULL) {
+    if ((ctx = RDB_alloc(p, sizeof(RdbxRespToFileWriter))) == NULL) {
         fclose(filePtr);
-        RDB_reportError(p, (RdbRes) RDBX_ERR_RESP_FAILED_ALLOC, "Failed to allocate struct RdbxRespFileWriter");
+        RDB_reportError(p, (RdbRes) RDBX_ERR_RESP_FAILED_ALLOC, "Failed to allocate struct RdbxRespToFileWriter");
         return NULL;
     }
 
