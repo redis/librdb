a135c99ce4e8ba9e945c8928fb4c8001aa2c4c81
Ozan Tezcan <ozantezcan@gmail.com>
2023-09-27 07:52:55 +0300

    delete unused

--- deps/redis/util.h
+++ deps/redis/util.h
@@ -18,15 +18,6 @@ int ll2string(char *s, size_t len, long long value);
 int ull2string(char *s, size_t len, unsigned long long value);
 int lpStringToInt64(const char *s, unsigned long slen, int64_t *value);
 unsigned int getEnvVar(const char* varName, unsigned int defaultVal);
-
-
-/* long double to string conversion options */
-typedef enum {
-    LD_STR_AUTO,     /* %.17Lg */
-    LD_STR_HUMAN,    /* %.17Lf + Trimming of trailing zeros */
-    LD_STR_HEX       /* %La */
-} ld2string_mode;
-
 double zzlStrtod(unsigned char *vstr, unsigned int vlen);
 int d2string(char *buf, size_t len, double value);
 
