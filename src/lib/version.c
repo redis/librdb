7a70fb055fc96635827b8ab66c35731bee178247
Moti Cohen <moticless@gmail.com>
2024-11-10 13:37:35 +0200

    Moving back API functions to parser.c

--- src/lib/version.c
+++ src/lib/version.c
@@ -2,7 +2,6 @@
 #include <assert.h>
 
 #include "version.h"
-#include "parser.h"
 
 #define STATIC_ASSERT(COND,MSG) typedef char static_assertion[(COND)?1:-1]
 
@@ -18,16 +17,3 @@ STATIC_ASSERT(
         (__builtin_strcmp(LIBRDB_VERSION_STRING, LIBRDB_COMPOSED_VERSION_STRING) == 0),
         "LIBRDB_VERSION_STRING does not match the individual version components"
 );
-
-/*** LIB API functions ***/
-
-_LIBRDB_API const char *RDB_getLibVersion(int *major, int *minor, int *patch) {
-    if (major) *major = LIBRDB_MAJOR_VERSION;
-    if (minor) *minor = LIBRDB_MINOR_VERSION;
-    if (patch) *patch = LIBRDB_PATCH_VERSION;
-    return LIBRDB_VERSION_STRING;
-}
-
-_LIBRDB_API int RDB_getMaxSuppportRdbVersion(void) {
-    return LIBRDB_SUPPORT_MAX_RDB_VER;
-}
